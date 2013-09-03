34a132bbdd3333e52da9db14458485e0a77c6f8e
Hobson Lane <hobsonlane@gmail.com>
2013-09-02 22:15:13 -0700

    add views

--- oregon/home/views.py
+++ oregon/home/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("This is the home page for storydecoder.com in the oregon.home app.")
+
+def index(request):
+    return HttpResponse("This is the idnex page for storydecoder.com in the oregon.home app.")
