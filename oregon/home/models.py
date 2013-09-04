b79c49d4b4b1832dca38671351e2102ecaf29150
Hobson Lane <hobsonlane@gmail.com>
2013-09-03 21:41:18 -0700

    remove words and homographs from home/models.py

--- oregon/home/models.py
+++ oregon/home/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 
-class Word(models.Model):
-    english = models.CharField(max_length=15, blank=False, unique=True)
-
-class Morpheme(models.Model):
-    """
-    Parameters and fit results associated with a variable in the database.
-
-    Not sure if a database is really needed for this, but is a nice-to-have
-    """
-    word = models.OneToOneField(Word, blank=False, null=False, unique=True)
-    bound = models.NullBooleanField(default=True, help_text="Whether the word is *not* \"free-standing\" as it's own word.")
 
 class User(models.Model):
     name = models.CharField(max_length=128)
