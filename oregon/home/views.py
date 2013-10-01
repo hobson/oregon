4cd3cc8696db25f531839dbda4c903357df27e58
Hobson Lane <hobsonlane@gmail.com>
2013-09-30 22:57:40 -0700

    graph doesn't work

--- oregon/home/views.py
+++ oregon/home/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 
 def graph(request):
     nodes = ["node 1", 'node2', 'node3', 'node4']
-    edges = [{'from': 1, 'to': 2},{'from': 2, 'to': 3},{'from': 3, 'to': 4}, {'from': 1, 'to': 3},{'from': 2, 'to': 4}]
+    edges = [{'source': 1, 'target': 2},{'source': 2, 'target': 3},{'source': 3, 'to': 0}, {'source': 1, 'target': 3},{'source': 2, 'target': 0}]
     return render(request, 'home/base.html', {'data': {'nodes': nodes, 'edges': edges}})
 
 def index(request):
